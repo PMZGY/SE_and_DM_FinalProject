fa62c1488164a0ff9eda5a9660c079cb2af5119e
andy tsai <yuanting_tsai1992@gmail.com>
2018-11-11 14:24:04 +0800

    test sourcetree版本

--- BackEndAdvance/MVC_HWK.Service/BookFactory.cs
+++ BackEndAdvance/MVC_HWK.Service/BookFactory.cs
@@ -15,7 +15,7 @@ namespace MVC_HWK.Service
 
             switch (Common.ConfigTool.GetAppsetting("DaoInTest"))
             {
-               // case "Y":
+               // case "Y":~
                 case "Y":
                     result = new MVC_HWK.Dao.BookTestDao();
                     break;
