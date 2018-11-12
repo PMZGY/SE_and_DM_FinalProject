0e5f5d23085a483d9ef54dac0afb6921383c234e
ponponmusic <anlyfish9@gmail.com>
2018-11-12 13:33:50 +0800

    # delete comment

--- BackEndAdvance/MVC_HWK.Service/BookFactory.cs
+++ BackEndAdvance/MVC_HWK.Service/BookFactory.cs
@@ -15,14 +15,6 @@ namespace MVC_HWK.Service
 
             switch (Common.ConfigTool.GetAppsetting("DaoInTest"))
             {
-                //test pull 16:22 
-                // test embedded-2
-
-                // case "PONPON"
-                // case "Y":
-
-                // test embedded
-
                 case "Y":
                     result = new MVC_HWK.Dao.BookTestDao();
                     break;
